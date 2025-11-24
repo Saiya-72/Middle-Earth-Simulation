b00b9ee0aedcce62b32bdd7f408e86e0c9edd30b
Josiah Brown <josiahd7.brown@gmail.com>
2025-11-24 13:54:24 -0500

    Added race class and world class

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-print("Simulation of Middle Earth")
+class Race():
+    def __init__(self, name, lifespan, reproduction_rate, fighting_ability):
+        self.name = name
+        self.lifespan = lifespan  # in years
+        self.reproduction_rate = reproduction_rate  # average number of offspring per year in thousands
+        self.fighting_ability = fighting_ability  # scale from 1 to 10
+
+class World():
+    def __init__(self, races_positions:dict):
+        self.races_positions = {} # for example: {"Elves": [(0, 0), (7, 8)], "Dwarves": [(1, 1), (12, 12)]}
+
+    def display_map():
+        for race, position in self.races_positions.items():
+            print()
+
+
+if __name__ == "__main__":
+    elves = Race("Elves", 1500, 0.1, 7)
+    dwarves = Race("Dwarves", 400, 1.0, 8)
+    orcs = Race("Orcs", 40, 3.0, 9)
+    humans = Race("Humans", 80, 10.0, 4)
+    hobbits = Race("Hobbits", 100, 2.0, 2)
+
+    
